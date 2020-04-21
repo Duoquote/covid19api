9e7c3e8fc9dcee090fde98c1f5217d51385c1249
Guven Degirmenci <guven4902000@hotmail.com>
2020-04-21 16:05:17 +0300

    Splitted the code to use in heroku.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const axios = require("axios"),
-      express = require("express"),
-      CronJob = require("cron").CronJob,
-      JSDOM = require("jsdom").JSDOM,
+const express = require("express"),
+      fs = require("fs"),
       Sentry = require("@sentry/node"),
       app = express()
 
@@ -11,72 +9,19 @@ if (process.env.SENT_DSN) {
   });
 }
 
-
-var dateDict = {
-  "OCAK": 1,
-  "ŞUBAT": 2,
-  "MART": 3,
-  "NİSAN": 4,
-  "MAYIS": 5,
-  "HAZİRAN": 6,
-  "TEMMUZ": 7,
-  "AĞUSTOS": 8,
-  "EYLÜL": 9,
-  "EKİM": 10,
-  "KASIM": 11,
-  "ARALIK": 12
-}
-
-var varDict = {
-  "TOPLAM TEST SAYISI": "total_tests",
-  "TOPLAM VAKA SAYISI": "total_cases",
-  "TOPLAM VEFAT SAYISI": "total_deaths",
-  "TOPLAM YOĞUN BAKIM HASTA SAYISI": "total_intense_care",
-  "TOPLAM ENTUBE HASTA SAYISI": "total_incubated",
-  "TOPLAM İYİLEŞEN HASTA SAYISI": "total_head"
-}
-
-var data;
-
-function updateData() {
-  axios.get("https://covid19.saglik.gov.tr/")
-    .then(resp=>{
-      var tmpData = {};
-      eval([...resp.data.matchAll(/(?<data>var config = [\s\S]*? data: {[\s\S]*?}),\r\n.*options/gm)][0].groups.data + "}");
-      let dailyData = config.data.labelsTooltip.reduce((obj, iter, i)=>{
-        let tarih = dateDict[iter.match(/\d+ ([A-ZİÜĞÇŞÖ]+)/)[1]].toString() + "-" + config.data.labels[i].toString() + "-" + iter.match(/.* (\d+)/)[1];
-
-        if (config.data.datasets[0].data[i]) {
-          obj[tarih] = {
-            date: new Date(tarih).getTime(),
-            cases: config.data.datasets[0].data[i],
-            deaths: config.data.datasets[1].data[i]
-          }
-        }
-        return obj;
-      }, {})
-      tmpData.dailyData = dailyData;
-      var dom = new JSDOM(resp.data).window.document;
-      tmpData.detail = {};
-      [...dom.querySelector("div[class='baslik-tablo']").nextElementSibling.querySelector("ul").children].forEach((elem)=>{
-        let label = elem.textContent.match(/(?!\d)([A-ZİÜĞÇŞÖ]+)/g).join(" ");
-        tmpData.detail[varDict[label]] = parseInt(elem.textContent.match(/(\d+\.?\d+|\d+)/g)[0].replace(".", ""));
-      })
-      data = tmpData;
-    })
-}
-
-updateData();
-
-var job = new CronJob("0 * * * *", updateData, null, true);
-
 if (process.env.SENT_DSN) {
   app.use(Sentry.Handlers.requestHandler());
 }
 
 
 app.get("/", (req, res)=>{
-  res.json(data);
+  if (fs.existsSync("data.json")) {
+    let data = fs.readFileSync("data.json");
+    res.json(JSON.parse(data));
+  } else {
+    res.send("Loading data, please send request later...");
+  }
+
 })
 
 if (process.env.SENT_DSN) {
---
+++ update_data.js
@@ -0,0 +1,76 @@
+const axios = require("axios"),
+      JSDOM = require("jsdom").JSDOM,
+      fs = require("fs"),
+      // CronJob = require("cron").CronJob,
+      Sentry = require("@sentry/node")
+
+if (process.env.SENT_DSN) {
+  Sentry.init({
+    dsn: process.env.SENT_DSN
+  });
+}
+
+var dateDict = {
+  "OCAK": 1,
+  "ŞUBAT": 2,
+  "MART": 3,
+  "NİSAN": 4,
+  "MAYIS": 5,
+  "HAZİRAN": 6,
+  "TEMMUZ": 7,
+  "AĞUSTOS": 8,
+  "EYLÜL": 9,
+  "EKİM": 10,
+  "KASIM": 11,
+  "ARALIK": 12
+}
+
+var varDict = {
+  "TOPLAM TEST SAYISI": "total_tests",
+  "TOPLAM VAKA SAYISI": "total_cases",
+  "TOPLAM VEFAT SAYISI": "total_deaths",
+  "TOPLAM YOĞUN BAKIM HASTA SAYISI": "total_intense_care",
+  "TOPLAM ENTUBE HASTA SAYISI": "total_incubated",
+  "TOPLAM İYİLEŞEN HASTA SAYISI": "total_head"
+}
+
+var data;
+
+function updateData() {
+  axios.get("https://covid19.saglik.gov.tr/")
+    .then(resp=>{
+      try {
+        var tmpData = {};
+        eval([...resp.data.matchAll(/(?<data>var config = [\s\S]*? data: {[\s\S]*?}),\r\n.*options/gm)][0].groups.data + "}");
+        let dailyData = config.data.labelsTooltip.reduce((obj, iter, i)=>{
+          let tarih = dateDict[iter.match(/\d+ ([A-ZİÜĞÇŞÖ]+)/)[1]].toString() + "-" + config.data.labels[i].toString() + "-" + iter.match(/.* (\d+)/)[1];
+
+          if (config.data.datasets[0].data[i]) {
+            obj[tarih] = {
+              date: new Date(tarih).getTime(),
+              cases: config.data.datasets[0].data[i],
+              deaths: config.data.datasets[1].data[i]
+            }
+          }
+          return obj;
+        }, {})
+        tmpData.dailyData = dailyData;
+        var dom = new JSDOM(resp.data).window.document;
+        tmpData.detail = {};
+        [...dom.querySelector("div[class='baslik-tablo']").nextElementSibling.querySelector("ul").children].forEach((elem)=>{
+          let label = elem.textContent.match(/(?!\d)([A-ZİÜĞÇŞÖ]+)/g).join(" ");
+          tmpData.detail[varDict[label]] = parseInt(elem.textContent.match(/(\d+\.?\d+|\d+)/g)[0].replace(".", ""));
+        })
+        data = tmpData;
+        fs.writeFileSync("data.json", JSON.stringify(data));
+      } catch(err) {
+        Sentry.captureException(err);
+      }
+    })
+    .catch(err=>{
+      Sentry.captureException(err);
+    })
+}
+
+// var job = new CronJob("0 * * * *", updateData, null, true);
+updateData()
